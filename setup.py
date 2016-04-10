062bcc88a67ec94303272372123fd94e9f86bfac
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-04-11 01:25:08 +0530

    Adding files for hosting

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PrefSyncService',
+      version='1.0.0',
+      description='Share File Service For Sharing Files Via Sublime Text',
+      author='Dinesh Kumar',
+      author_email='dineshkumar_cse@hotmail.com',
+      url='http://http://sharefiles-liveasdev.rhcloud.com/',
+      install_requires=[ 'bottle', 'pymongo'],
+)
